7d3f7a866d75bc193a705a4b9fceefd057d9497a
maitreebain <maitreebain@gmail.com>
2021-08-04 17:53:06 -0400

    set up ui for search controller

--- NASA-App/SearchControllers/SearchViewController.swift
+++ NASA-App/SearchControllers/SearchViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet private weak var imageSearchBar: UISearchBar!
+    @IBOutlet private weak var imageCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UIUpdate()
+        imageSearchBar.delegate = self
+        imageCollectionView.delegate = self
+        imageCollectionView.dataSource = self
     }
-
-
+    
+    private func UIUpdate() {
+        
+    }
+    
+    
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell()
+    }
+    
+    
+}
